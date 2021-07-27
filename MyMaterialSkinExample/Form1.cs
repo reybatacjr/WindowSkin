6f20000b4a38305fd8713add1b8093c205304f45
Rey <reybatacjr@gmail.com>
2021-07-27 14:06:01 +0800

    Init

---
+++ MyMaterialSkinExample/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WindowSkin.Controls;
+
+namespace MyMaterialSkinExample
+{
+    public partial class Form1 : WindowSkinForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
